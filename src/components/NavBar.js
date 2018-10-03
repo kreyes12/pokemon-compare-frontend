29fb1def831e72786fc916d3f1f6de8185df9428
Casey Van-Roose <caseyvanroose@Caseys-MacBook-Pro.local>
2018-10-03 09:21:33 +0100

    styled nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,27 +7,30 @@ class NavBar extends React.Component {
     return (
       <nav className='navbar is-transparent test' aria-label='main navigation'>
         <div className='navbar-brand'>
-          <a href='#' className='navbar-item'>
-                Pokemon Compare
-          </a>
-          {props.currentUser && `Hello, Trainer ${props.currentUser}.`}
-          {
-            props.currentUser 
-            ? <button onClick={props.logout}>LOGOUT</button>
-            :             <React.Fragment>
-                <Link to='/login'>
-                  <button>LOGIN</button>
-                </Link>
-                <Link to='/register'>
-                  <button>REGISTER</button>
-                </Link>
-              </React.Fragment>
-          }
+            <a href='#' className='navbar-item'>
+                    <h1>Pokemon Compare</h1>
+            </a>
         </div>
-        <div className='navbar-menu'>
-          <a href='#' className='navbar-end' />
-        </div>
+        <div className="navbar-start"></div>
+        <div class="navbar-end">
+            <div className="navbar-item">
 
+            {props.currentUser && `Hello, Trainer ${props.currentUser}.`}
+            
+            {
+                props.currentUser 
+                ? <button className="button" onClick={props.logout}>LOGOUT</button>
+                :             <React.Fragment>
+                    <Link to='/login'>
+                    <button>LOGIN</button>
+                    </Link>
+                    <Link to='/register'>
+                    <button>REGISTER</button>
+                    </Link>
+                </React.Fragment>
+            }
+            </div>
+        </div>
       </nav>
     )
   }
